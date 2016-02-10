f5abb43998e6de9b9a2f4805e0c104327830190d
Amaan <amaanbadruddin2018@u.northwestern.edu>
2016-02-10 14:45:36 -0600

    commit lab6

--- main.cpp
+++ main.cpp
@@ -29,21 +29,27 @@ void populate_db() // function definition
     Student s_1("Miguel", 22, "EECS");
     Student s_2("Tina", 20, "Math");
     Student s_3("Mike", 18, "Chemical Engineering");
+    Student s_4("Amaan", 18, "Industrial Engineering");
+    Student s_5("Evan", 19, "Industrial Engineering");
 
     //Add Students
     CAESAR.add_student(s_1);
     CAESAR.add_student(s_2);
     CAESAR.add_student(s_3);
+    CAESAR.add_student(s_4);
+    CAESAR.add_student(s_5);
 
     //Construct Courses
-    Course algorithms(333, "EECS");
-    Course programming(230, "EECS");
-    Course algebra(101, "Math");
+    Course algorithms(333, "EECS", "Algo");
+    Course programming(230, "EECS", "Program");
+    Course algebra(101, "Math", "Easy");
+    Course optimization(313, "IE", "Deterministic Models");
 
     //Add Courses
     CAESAR.add_course(algorithms);
     CAESAR.add_course(programming);
     CAESAR.add_course(algebra);
+    CAESAR.add_course(optimization);
 
     //Construct Enrollments
     Enroll e(s_1,algorithms, 3.9);
@@ -51,6 +57,8 @@ void populate_db() // function definition
     Enroll e3(s_1,algebra, 3);
     Enroll e4(s_2,algorithms, 3.5);
     Enroll e5(s_3,algorithms, 3.0);
+    Enroll e6(s_4,optimization, 1.0);
+    Enroll e7(s_5,optimization, 4.0);
 
     //Add Enrollments
     CAESAR.add_enrollment(e);
@@ -58,6 +66,8 @@ void populate_db() // function definition
     CAESAR.add_enrollment(e3);
     CAESAR.add_enrollment(e4);
     CAESAR.add_enrollment(e5);
+    CAESAR.add_enrollment(e6);
+    CAESAR.add_enrollment(e7);
 }
 
 void run_two_tests()
